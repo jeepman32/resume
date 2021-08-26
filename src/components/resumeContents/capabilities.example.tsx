59029fc7502a6dd002809b397864c9f188aca7dd
Hayden Johal <hjohal@slq.qld.gov.au>
2021-08-26 20:13:29 +1000

    Better example configs

---
+++ src/components/resumeContents/capabilities.example.tsx
@@ -0,0 +1,15 @@
+const techCapabilities: {
+  [key in "proficient" | "intermediate" | "learning"]: {
+    [discipline: string]: string[];
+  };
+} = {
+  proficient: {
+    key: ["", "", ""],
+  },
+  intermediate: {
+    key: ["", "", ""],
+  },
+  learning: {
+    key: ["", "", ""],
+  },
+};
---
+++ src/components/resumeContents/contact.example.tsx
@@ -0,0 +1,4 @@
+const contact = {
+  email: "",
+  phone: "",
+};
