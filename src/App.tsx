9b1f1afa07e7dc4656bff2204bc56dbd738795dd
Hayden Johal <hjohal@slq.qld.gov.au>
2021-08-26 20:49:07 +1000

    Blurb and interests

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import Grid from "@material-ui/core/Grid";
 
 import CodePaper from "./components/CodePaper";
 
+/* eslint-disable import/no-webpack-loader-syntax */
+// @ts-ignore
+import attributes from "!!raw-loader!./components/resumeContents/attributes.tsx";
+// @ts-ignore
+import contact from "!!raw-loader!./components/resumeContents/contact.tsx";
+/* eslint-enable import/no-webpack-loader-syntax */
+
 const App = () => {
   return (
     <div style={{ width: "210mm", padding: "0.5in" }}>
-      <Grid container spacing={4}>
+      <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Typography variant="h1">Lorem ipsum.</Typography>
+          <Typography variant="h1">title</Typography>
         </Grid>
         <Grid item xs={5}>
-          <CodePaper codeString={""} />
+          <CodePaper codeString={contact} />
         </Grid>
         <Grid
           item
@@ -20,12 +27,10 @@ const App = () => {
           justifyContent="center"
           alignItems="center"
         >
-          <Typography>
-            Lorem ipsum.
-          </Typography>
+          <Typography>something???</Typography>
         </Grid>
         <Grid item xs={12}>
-          <CodePaper codeString={""} />
+          <CodePaper codeString={attributes} />
         </Grid>
         <Grid item xs={12}>
           <Typography variant="h3">Prior Experience</Typography>
