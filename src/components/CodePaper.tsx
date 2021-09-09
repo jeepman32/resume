9281b984559c168b63505f6867deb81d63668a5a
Hayden Johal <hjohal@slq.qld.gov.au>
2021-09-09 22:37:49 +1000

    Final changes

--- src/components/CodePaper.tsx
+++ src/components/CodePaper.tsx
@@ -2,10 +2,10 @@ import Paper from "@material-ui/core/Paper";
 import styled from "@material-ui/core/styles/styled";
 
 import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import ts from "react-syntax-highlighter/dist/esm/languages/prism/typescript";
+import typescript from "react-syntax-highlighter/dist/esm/languages/prism/typescript";
 import prism from "react-syntax-highlighter/dist/esm/styles/prism/prism";
 
-SyntaxHighlighter.registerLanguage("ts", ts);
+SyntaxHighlighter.registerLanguage("typescript", typescript);
 
 const StyledPaper = styled(Paper)(({ theme: { spacing } }) => ({
   borderRadius: spacing(1),
@@ -13,15 +13,17 @@ const StyledPaper = styled(Paper)(({ theme: { spacing } }) => ({
 }));
 
 const CodePaper = ({ codeString }: { codeString: string }) => {
+  console.log({ prism });
   return (
     <StyledPaper elevation={5}>
       <SyntaxHighlighter
         customStyle={{
-          fontSize: "11pt",
+          fontSize: "10.5pt",
           padding: "2em",
           margin: "-1em",
+          fontFamily: `"IBM Plex mono", monospace`
         }}
-        language="ts"
+        language="typescript"
         style={prism}
       >
         {codeString}
